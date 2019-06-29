bb578a8b9fa7ea3fde9b6caf51f51f3b9cee6a82
sharktamer <donohoe.shane@gmail.com>
2019-06-29 18:52:58 +0100

    add function and test for applying colours to names

--- sopel_modules/birthdays/birthdays.py
+++ sopel_modules/birthdays/birthdays.py
@@ -3,7 +3,10 @@
 from __future__ import (unicode_literals, absolute_import,
                         division, print_function)
 
-from sopel import module
+import itertools
+
+import sopel.module
+import sopel.formatting
 import requests
 
 
@@ -16,10 +19,23 @@ def setup(bot):
 
 
 def get_names(j):
+    """ Get names from json response """
     return [i['text'] for i in j['data']['Births']]
 
 
-@module.commands('bdays')
+def apply_colours(days, colours):
+    """ Apply given colours to list of names """
+    cdays = [sopel.formatting.color(i, j)
+             for i, j in zip(days, itertools.cycle(colours))]
+
+    return cdays
+
+
+@sopel.module.commands('bdays')
 def bdays(bot, trigger):
     r = requests.get('https://history.muffinlabs.com/date')
     rj = r.json()
+
+    names = get_names(rj)
+
+    bot.say(', '.join(names))
